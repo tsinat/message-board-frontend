65bd358f6f92cf9dbe2bc288e85853226ebeff48
Tsinat Zeree <cheru87@gmail.com>
2016-06-02 23:45:08 -0700

    refactor

--- public/main.js
+++ public/main.js
@@ -1,3 +1,23 @@
-$(function(){
-    
+$(function() {
+    $('.form').submit(sendMessage);
+    console.log('It is working');
 });
+
+function sendMessage(e) {
+    e.preventDefault();
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var image = $('#image').val();
+    var urlMod = image.substr(4);
+    console.log(urlMod);
+    var message = $('#message').val();
+
+    $.get(`/message/${name}/${email}/${urlMod}/${message}`)
+        .done(function(data) {
+            console.log(data);
+        })
+        .fail(function(error) {
+            console.log(error);
+        });
+    console.log('message sent');
+}
--- server.js
+++ server.js
@@ -10,46 +10,52 @@ var moment = require('moment');
 let nodeStatic = require('node-static');
 var myDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 let file = new nodeStatic.Server('./public');
-let myData = [{
-    body: "Hey, isn't this Jade thing supposed to make our jobs easier",
-    date: myDate,
-    image: 'http://i.imgur.com/k37hPI3.jpg'
-    },
-    {
-    body: "I don't know, why are you asking me?",
-    date: myDate,
-    image: 'http://i.imgur.com/CyHNBdM.jpg'
-    },{
-    body: "guys, don't worry it will get easier once u got uset to it",
-    date: myDate,
-    image: 'http://i.imgur.com/IKOQUI0.jpg'
-}]
 
-http.createServer((req, res)=>{
-    let html;
-    let qsParts = req.url.split('?');
-    let path = qsParts[0];
+http.createServer((req, res) => {
+        var temp = encodeURIComponent(req.url);
+        console.log('rowUrl:', req.url);
+        console.log('encoded:', temp);
+        let myData = [{
+            body: "Hey, isn't this Jade thing supposed to make our jobs easier",
+            date: myDate,
+            image: 'http://i.imgur.com/k37hPI3.jpg'
+        }, {
+            body: "I don't know, why are you asking me?",
+            date: myDate,
+            image: 'http://i.imgur.com/CyHNBdM.jpg'
+        }, {
+            body: "guys, don't worry it will get easier once u got uset to it",
+            date: myDate,
+            image: 'http://i.imgur.com/IKOQUI0.jpg'
+        }]
 
-    switch(path){
-        case '/':{
-            html = jade.renderFile('./views/index.jade');
-            res.end(html);
-            break;
+        let html;
+        let qsParts = req.url.split('?');
+        let path = qsParts[0];
+
+        switch (path) {
+            case '/':
+                {
+                    html = jade.renderFile('./views/index.jade');
+                    res.end(html);
+                    break;
+                }
+            case '/message':
+                {
+                    html = jade.renderFile('./views/message.jade', {
+                        messages: myData
+                    });
+                    res.end(html);
+                }
         }
-        case '/message':{
-            html = jade.renderFile('./views/message.jade',{
-                messages: myData
-            });
-            res.end(html);
-        }
-    }
 
-    file.serve(req, res);
-})
-.listen(PORT, err =>{
-    if(err) return console.log(err);
-    console.log(`Node server listening on port ${PORT}`);
-});
-function getMyData(value){
+        file.serve(req, res);
+    })
+    .listen(PORT, err => {
+        if (err) return console.log(err);
+        console.log(`Node server listening on port ${PORT}`);
+    });
+
+function getMyData(value) {
     return value;
 }
